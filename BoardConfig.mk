f661bf29c08ced6010c7a65a4e318d69fff5da25
听雨 <1853821818@qq.com>
2026-08-26 01:21:54 +0800

    Fix: add BOARD_PREBUILT_DTBIMAGE_DIR for dtb.img

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,7 +40,7 @@ BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x07c08000 -
 # DTBO
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_PREBUILT_DTBOIMAGE := device/xiaomi/cannon/prebuilt/dtbo.img
-BOARD_INCLUDE_DTB_IN_BOOTIMG := false
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_PREBUILT_DTBIMAGE_DIR := device/xiaomi/cannon/prebuilt
 
 # Recovery
