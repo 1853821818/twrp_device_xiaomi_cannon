3572b9c74a95a9f3855a0bdaf355bb0239fd839a
听雨 <1853821818@qq.com>
2026-08-26 01:41:03 +0800

    Fix: simplify device.mk, fix PRODUCT_COPY_FILES path

--- device.mk
+++ device.mk
@@ -16,14 +16,11 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    fastbootd \
-    android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.1-impl-mock
+    fastbootd
 
-# Recovery
+# Recovery files - copy to recovery ramdisk root
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/fstab.mt6853:$(TARGET_COPY_OUT_RECOVERY)/root/fstab.mt6853 \
-    $(LOCAL_PATH)/recovery/init.recovery.mt6853.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.mt6853.rc
+    $(LOCAL_PATH)/recovery/init.recovery.mt6853.rc:recovery/root/init.recovery.mt6853.rc
 
 # USB
 PRODUCT_PACKAGES += \
@@ -33,11 +30,3 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
-
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
-# Permissions
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.ims.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/handheld_core_hardware.xml
